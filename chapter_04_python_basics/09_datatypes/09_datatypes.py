4e26434ebda66199d1d6f0f799c27f3fe9a3f805
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2026-01-03 11:16:50 +0330

    session 9 added

---
+++ chapter_04_python_basics/09_datatypes/09_datatypes.py
@@ -0,0 +1,23 @@
+# Numeric Types
+# int => 0 , 5 , -10
+# float => 1.5 , 3.4 , 7.9
+# complex
+
+age = 10 # int
+height = 1.80 # float
+
+
+# String / str
+first_name = "Arvin"
+last_name = 'Maroufi'
+
+
+# Boolean
+is_student = True
+has_car = False
+
+
+print(type(10))
+print(type('hello'))
+print(type(1.7))
+
